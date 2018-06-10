0008f16b3af715715c8efaca220bb2c5880bc5f4
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-06-10 16:01:30 +0300

    [LTB-32] Improve Message matchers design and docs

--- code/network/lib/Loot/Network/Message.hs
+++ code/network/lib/Loot/Network/Message.hs
@@ -5,7 +5,6 @@
 {-# LANGUAGE TypeFamilies              #-}
 {-# LANGUAGE TypeFamilyDependencies    #-}
 
--- TODO document better
 -- | Messages and message-dependent callbacks matching.
 
 module Loot.Network.Message
@@ -13,53 +12,62 @@ module Loot.Network.Message
        , Callback (..)
        , CallbackWrapper (..)
        , handler
-       , createDMap
-       , runCallback
-       , runCallbackInt
+       , handlerDecoded
+       , runCallbacks
+       , runCallbacksInt
        ) where
 
-import Codec.Serialise (Serialise (..), deserialise)
+import Codec.Serialise (DeserialiseFailure, Serialise (..), deserialiseOrFail)
 import qualified Data.ByteString.Lazy as BSL
 import Data.Dependent.Map (DSum ((:=>)))
 import qualified Data.Dependent.Map as D
 import Data.GADT.Compare ((:~:) (Refl), GEq (..), GOrdering (..))
 import Data.Reflection (reifyNat)
 import Data.Singletons.TypeLits hiding (natVal)
-import Unsafe.Coerce (unsafeCoerce)
+import Data.Tagged (Tagged (..))
+import Data.Type.Equality (testEquality)
 
 -- | Message is a type that has unique message type (expressed by
 -- 'Nat' type family instance).
 class (KnownNat (MsgTag d), Serialise d) => Message d where
     type family MsgTag d = (n :: Nat) | n -> d
 
--- | Action called on a particular type.
+-- | Action called on a particular type (can be either parse error or type itself).
 data Callback m a n =
     forall d. (Message d, MsgTag d ~ n) =>
-    Callback { unCallback :: d -> m a }
+    Callback { unCallback :: Tagged d ByteString -> m a }
 
 -- | Packed callback.
 data CallbackWrapper m a =
     forall n. KnownNat n => CallbackWrapper { unCallbackWrapper :: Callback m a n }
 
--- | Convenient helper to create callback wrappers.
-handler :: Message d => (d -> m a) -> CallbackWrapper m a
+-- | Convenient helper to create callback wrappers (from raw bytestring input).
+handler :: Message d => (Tagged d ByteString -> m a) -> CallbackWrapper m a
 handler = CallbackWrapper . Callback
 
--- Is there a better way to do this w/o using unsafeCoerce?
+-- | Callback wrapper from function processing parsed value.
+handlerDecoded ::
+       forall d m a. Message d
+    => (Either DeserialiseFailure d -> m a)
+    -> CallbackWrapper m a
+handlerDecoded action =
+    handler $ \(Tagged bs :: Tagged d ByteString) ->
+                action (deserialiseOrFail $ BSL.fromStrict bs)
+
 instance GEq (Sing :: Nat -> *) where
-    geq (SNat :: SNat n) (SNat :: SNat m) =
-        if natVal (Proxy @n) /= natVal (Proxy @m)
-        then Nothing
-        else Just (unsafeCoerce Refl)
+    geq = testEquality
 
 instance D.GCompare (Sing :: Nat -> *) where
-    gcompare (SNat :: SNat n) (SNat :: SNat m) =
-        case compare (natVal (Proxy @n)) (natVal (Proxy @m)) of
-          LT -> GLT
-          EQ -> unsafeCoerce GEQ
-          GT -> GGT
+    gcompare s1@(SNat :: SNat n) s2@(SNat :: SNat m) =
+        case testEquality s1 s2 of
+          Just Refl -> GEQ
+          Nothing ->
+            case compare (natVal (Proxy @n)) (natVal (Proxy @m)) of
+              LT -> GLT
+              GT -> GGT
+              EQ -> error "GCompare Sing: 'testEquality' and 'compare' are inconsistent"
 
--- | Creates a dmap from callbacks. If more than one callback with the
+-- Creates a dmap from callbacks. If more than one callback with the
 -- same incoming message typewill be in the list, only first will be
 -- put in.
 createDMap :: forall m a. [CallbackWrapper m a] -> D.DMap Sing (Callback m a)
@@ -68,15 +76,21 @@ createDMap callbacks =
     map (\(CallbackWrapper c@(_ :: Callback m a n)) -> ((SNat :: SNat n) :=> c))
         callbacks
 
--- | Executes a callback given a dmap, switching integer and BS.
-runCallback :: D.DMap Sing (Callback m a) -> SNat n -> ByteString -> m a
-runCallback dmap sn bs =
-    case dmap D.! sn of (Callback x) -> x (deserialise (BSL.fromStrict bs))
+-- | Executes a callback given a dmap, switching integer and BS. User
+-- must ensure that there is a callback that can be called, otherwise
+-- (in case of lookup failure) this function will "error".
+runCallbacks :: [CallbackWrapper m a] -> SNat n -> ByteString -> m a
+runCallbacks cbs sn bs =
+    case D.lookup sn dmap of
+      Just (Callback x) -> x (Tagged bs)
+      Nothing           -> error "runCallback: lookup in DMap failed"
+  where
+    dmap = createDMap cbs
 
 -- | Same as 'runCallback', but accepts value-level integer.
-runCallbackInt :: D.DMap Sing (Callback m a) -> Integer -> ByteString -> m a
-runCallbackInt dmap i bs =
-    reifyNat i $ \(Proxy :: Proxy n) -> runCallback dmap (SNat :: SNat n) bs
+runCallbacksInt :: [CallbackWrapper m a] -> Integer -> ByteString -> m a
+runCallbacksInt cbs i bs =
+    reifyNat i $ \(Proxy :: Proxy n) -> runCallbacks cbs (SNat :: SNat n) bs
 
 ----------------------------------------------------------------------------
 -- Testing
@@ -103,7 +117,7 @@ instance Message Msg3 where type MsgTag Msg3 = 3
 
 _test :: IO ()
 _test = do
-    let r1 = handler $ \(Msg1 s) -> putStrLn s
-    let r2 = handler $ \(Msg2 i) -> putText "wat?" >> print (i + 1)
-    let dmap = createDMap [r1,r2]
-    runCallback dmap (SNat :: SNat 3) "aoeu"
+    let create foo = handlerDecoded $ either (const $ putText "can't parse") foo
+    let r1 = create $ \(Msg1 s) -> putStrLn s
+    let r2 = create $ \(Msg2 i) -> putText "wat?" >> print (i + 1)
+    runCallbacksInt [r1,r2] 2 "aoeu"
