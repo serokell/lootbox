8a27f3e5e10a360078ef7885db65afeff150b819
Andrey Demidenko <andrey.demidenko@serokell.io>
2021-07-20 14:55:11 +0300

    [LTB-61] Add ToJSON instance for config file
    Problem:
    We have FromJSON instance so we can parse configuration from valid
    json or yaml. But there are cases when we want print this config file,
    send, log, etc. So it's useful to have ToJSON instance too.
    
    Solution:
    Add ToJSON instance for ConfigRec, converting to json happens with
    help of OptionsToJson class which recursively goes through config file
    and convert values.
    
    Note: we independently implemented `ToJSON` in this PR before we noticed #53

--- code/config/lib/Loot/Config/Yaml.hs
+++ code/config/lib/Loot/Config/Yaml.hs
@@ -2,26 +2,31 @@
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
+{-# LANGUAGE AllowAmbiguousTypes  #-}
 {-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE MonoLocalBinds       #-}
 
--- | Utilities for reading configuration from a file.
+-- | Utilities for parsing and serializing configuration with Yaml/JSON.
 module Loot.Config.Yaml
        ( configParser
        ) where
 
-import Data.Aeson (FromJSON (parseJSON))
+import Data.Aeson (FromJSON (parseJSON), ToJSON (toJSON), Value (Object))
 import Data.Aeson.BetterErrors (Parse, fromAesonParser, keyMay, keyOrDefault, toAesonParser')
-import Data.Vinyl (Rec ((:&), RNil))
+import Data.Aeson.Types (Object)
+import Data.Vinyl (Rec (RNil, (:&)))
 import GHC.TypeLits (KnownSymbol, symbolVal)
 
-import Loot.Config.Record ((:::), (::<), (::+), (::-), ConfigKind (Partial),
-                           ConfigRec, Item (ItemOptionP, ItemSub, ItemSumP, ItemBranchP),
-                           ItemKind, SumSelection)
+import qualified Data.HashMap.Strict as HM
 
+import Loot.Config.Record ((:::), (::<), (::+), (::-), ConfigKind (..),
+                           ConfigRec, Item (..), ItemKind, SumSelection)
+
+-- | Helper function to get config key from type level
+getConfigKey :: forall l . KnownSymbol l => Text
+getConfigKey = fromString $ symbolVal (Proxy :: Proxy l)
 
 -- | This class is almost like 'FromJSON' but uses @aeson-better-errors@.
 class OptionsFromJson (is :: [ItemKind]) where
@@ -41,7 +46,7 @@ instance
     configParser = (:&) <$> fmap ItemOptionP parseOption <*> configParser
       where
         parseOption :: Parse e (Maybe v)
-        parseOption = keyMay (fromString $ symbolVal (Proxy :: Proxy l)) fromAesonParser
+        parseOption = keyMay (getConfigKey @l) fromAesonParser
 
 instance
     forall l us is.
@@ -53,7 +58,7 @@ instance
     => OptionsFromJson ((l ::< us) ': is)
   where
     configParser = (:&)
-        <$> fmap ItemSub (parseMulti (Proxy :: Proxy l))
+        <$> fmap ItemSub (parseMulti @l)
         <*> configParser
 
 instance
@@ -66,7 +71,7 @@ instance
     => OptionsFromJson ((l ::+ us) ': is)
   where
     configParser = (:&)
-        <$> fmap ItemSumP (parseMulti (Proxy :: Proxy l))
+        <$> fmap ItemSumP (parseMulti @l)
         <*> configParser
 
 instance
@@ -79,7 +84,7 @@ instance
     => OptionsFromJson ((l ::- us) ': is)
   where
     configParser = (:&)
-        <$> fmap ItemBranchP (parseMulti (Proxy :: Proxy l))
+        <$> fmap ItemBranchP (parseMulti @l)
         <*> configParser
 
 -- | Internal function to parse multiple 'Item's
@@ -89,9 +94,67 @@ parseMulti
         , Monoid (ConfigRec 'Partial us)
         , OptionsFromJson us
         )
-    => Proxy l
-    -> Parse e (ConfigRec 'Partial us)
-parseMulti p = keyOrDefault (fromString $ symbolVal p) mempty configParser
+    => Parse e (ConfigRec 'Partial us)
+parseMulti = keyOrDefault (getConfigKey @l) mempty configParser
 
 instance OptionsFromJson is => FromJSON (ConfigRec 'Partial is) where
     parseJSON = toAesonParser' configParser
+
+-- Helper function to insert values in a JSON 'Object' by config key.
+insert' :: forall l v . (KnownSymbol l, ToJSON v) => v -> Object -> Object
+insert' value = HM.insert (getConfigKey @l) (toJSON value)
+
+-- | This class is helper which converts config to object
+class OptionsToJson (k :: ConfigKind) (is :: [ItemKind]) where
+  -- | Convert 'ConfigRec' to 'Object'
+  configToObject :: ConfigRec k is -> Object
+
+instance OptionsToJson (k :: ConfigKind) '[] where
+  configToObject RNil = HM.empty
+
+instance
+  forall l v is k
+  . ( KnownSymbol l
+    , ToJSON v
+    , OptionsToJson k is
+    )
+  => OptionsToJson k ((l ::: v) ': is) where
+  configToObject (ItemOptionP Nothing :& vs) = configToObject vs
+  configToObject (ItemOptionP (Just v) :& vs) = insert' @l v $ configToObject vs
+  configToObject (ItemOptionF v :& vs) = insert' @l v $ configToObject vs
+
+instance
+  forall l us is k
+  . ( KnownSymbol l
+    , Monoid (ConfigRec 'Partial us)
+    , OptionsToJson k us
+    , OptionsToJson k is
+    )
+  => OptionsToJson k ((l ::< us) ': is) where
+  configToObject (ItemSub v :& vs) = insert' @l v $ configToObject vs
+
+instance
+  forall l us is k
+  . ( KnownSymbol l
+    , Monoid (ConfigRec 'Partial (SumSelection l : us))
+    , OptionsToJson k (SumSelection l : us)
+    , OptionsToJson k is
+    )
+  => OptionsToJson k ((l ::+ us) ': is) where
+  configToObject (ItemSumP v :& vs) = insert' @l v $ configToObject vs
+  configToObject (ItemSumF v :& vs) = insert' @l v $ configToObject vs
+
+instance
+  forall l us is k
+  . ( KnownSymbol l
+    , Monoid (ConfigRec 'Partial us)
+    , OptionsToJson k us
+    , OptionsToJson k is
+    )
+  => OptionsToJson k ((l ::- us) ': is) where
+  configToObject (ItemBranchP v :& vs) = insert' @l v $ configToObject vs
+  configToObject (ItemBranchF Nothing :& vs) = configToObject vs
+  configToObject (ItemBranchF (Just v) :& vs) = insert' @l v $ configToObject vs
+
+instance OptionsToJson k is => ToJSON (ConfigRec k is) where
+  toJSON = Object . configToObject
